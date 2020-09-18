5bd705e5fa2796890a59cb84319622decf81cad9
Tiago Costa <tiago.tagcosta@gmail.com>
2020-09-18 00:12:00 -0300

    adicionado calcHamming( )

--- hamming/Hamming.java
+++ hamming/Hamming.java
@@ -6,7 +6,7 @@ import java.util.BitSet;
 public class Hamming {
 
     public static byte[] encode(byte[] data) {
-        ArrayList<Byte> decoded = new ArrayList<>();
+        ArrayList<Byte> resultBytes = new ArrayList<>();
         BitSet codeword = new BitSet();
         int bitPosition = 0;
         
@@ -14,31 +14,52 @@ public class Hamming {
         for(int index = 2; index < data.length; index++) {
             BitSet bits = BitSet.valueOf(new long[] { data[index] });
             
-            System.out.println("bits: "+Integer.toBinaryString(bits.cardinality()));
+            System.out.println("index: "+index);
+            System.out.println("bits: "+bits.toString());
             // até 4 bits, vai registrando os bits no codeword
-            if(bitPosition < 4) {
-                if(bits.get(index)) {
-                    codeword.flip(bitPosition);
+            for(int i = 7; i >= 0; i--) {
+                if(bitPosition == 4) {
+                    // calcula o hamming
+                    resultBytes.add(calcHamming(codeword));
+                    // salva o resultado no resultBytes, e recomeça
+                    bitPosition = 0;
                 }
-                System.out.println("i: "+bitPosition+" codeword: "+codeword.toString());
+                if(bits.get(i)) {
+                    codeword.set(bitPosition);
+                }
+                System.out.println("i: "+i+" codeword: "+codeword.toString());
                 bitPosition++;
-            } else {
-                // calcula o hamming
-                decoded.add(calcHamming(codeword));
-                // salva o resultado no decoded, e recomeça
-                bitPosition = 0;
-            }
+            } 
         }
 
-        System.out.println("array: "+decoded.toString());
-        return null;
+        System.out.println("array: "+resultBytes.toString());
+        byte[] result = new byte[resultBytes.size()];
+
+        for (int i = 0; i < result.length; i++) {
+            result[i] = resultBytes.get(i);
+        }
+        return result;
     }
 
     private static byte calcHamming(BitSet codeword) {
+        for(int i=0; i<8; i++) {
+            System.out.println(codeword.get(i));
+        }
         // para cada bit, soma bit(i) com bit(i+1)
-        // se soma = impar(1), res=1
-        // senao, res=0
-        return 0;
+        for(int i=0; i<3; i++) {
+            // se soma = impar(1), res=1
+            // senao, res=0
+            if(codeword.get(i) && !codeword.get(i+1) || !codeword.get(i) && codeword.get(i+1)) {
+                codeword.set(i+4);
+            }
+        }
+
+        for(int i=0; i<8; i++) {
+            System.out.println(codeword.get(i));
+        }
+        byte[] coded = codeword.toByteArray();
+        
+        return coded[0];
     }
 
 }
